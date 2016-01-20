2732ae8c04ed715475648e463919e53b4016af23
Abigail Rolling and torrence stratton <tlstratton@gmail.com>
2016-01-20 15:11:39 -0800

    Find maximum testable prime

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
-var primeFinder = function(limit) {
+var populateArray = function(limit) {
   var limitArray = [];
   for (var i=0; i <= limit; i++) {
     limitArray.push(i);
   }
+  console.log(limitArray);
   return limitArray;
+}
+
+var rootFinder = function(limit) {
+  for (var i=2; i < limit; i++) {
+    if (i * i > limit) {
+      console.log(i);
+      return i;
+    }
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,11 @@
-describe ("primeFinder", function () {
+describe ("populateArray", function () {
   it("will create an array containing all numbers from 0 to N", function() {
-    expect(primeFinder(5)).to.eql([0, 1, 2, 3, 4, 5]);
+    expect(populateArray(5)).to.eql([0, 1, 2, 3, 4, 5]);
   });
-//it("will test (prime) to see if prime^2 > n", function() {
-//    expect(primeFinder(25)).to.equal(6);
-//  });
+});
+
+describe ("rootFinder", function () {
+  it("will test (prime) to see if prime^2 > n", function() {
+   expect(rootFinder(25)).to.equal(6);
+ });
 });
